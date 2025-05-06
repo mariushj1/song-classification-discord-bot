89df3e83cedc8d63c40b28d761a1e70dd9fdf3d5
mariushj1 <marius_jacobsen@hotmail.dk>
2025-05-06 20:26:19 +0200

    added sentiment calculation

--- song_classification.py
+++ song_classification.py
@@ -5,12 +5,14 @@ import re
 import lyricsgenius
 import pandas as pd
 from collections import Counter
+import nltk
+from nltk.corpus import stopwords
 
 # Load environment variables from .env file
 load_dotenv()
 
 # Define the youtube link and id
-yt_url = "https://www.youtube.com/watch?v=RBumgq5yVrA"
+yt_url = "https://www.youtube.com/watch?v=oofSnsGkops"
 yt_id = re.search(r"(?:v=)([a-zA-Z0-9_-]{11})", yt_url)
 yt_id = yt_id.group(1)
 
@@ -65,12 +67,39 @@ else:
 
 cleaned_lyrics = re.sub(r'\[.*?\]\s*', '', cleaned_lyrics)
 cleaned_lyrics = cleaned_lyrics.replace("'", "")
-
-# Using NLP to determine lyrics sentiment
 cleaned_lyrics = cleaned_lyrics.lower()
 
+# Tokenize lyrics
 words = re.findall(r'\b\w+\b', cleaned_lyrics)
 word_counts = Counter(words)
-df = pd.DataFrame(word_counts.items(), columns=['word', 'count'])
-df = df.sort_values(by='count', ascending=False).reset_index(drop=True)
-print(df)
+word_df = pd.DataFrame(word_counts.items(), columns=['word', 'count'])
+word_df = word_df.sort_values(by='count', ascending=False).reset_index(drop=True)
+
+# Importing and remove stop word
+nltk.download('stopwords')
+stop_words = set(stopwords.words('english'))
+word_df = word_df[~word_df['word'].isin(stop_words)]
+
+# Getting AFINN sentiment lexicon
+file_path = "AFINN-111.txt"
+afinn_data = []
+
+with open(file_path, 'r') as f:
+    for line in f:
+        word, score = line.strip().split("\t") 
+        afinn_data.append([word, float(score)]) 
+
+afinn_df = pd.DataFrame(afinn_data, columns=['word', 'sentiment'])
+# Manually edit words
+afinn_df = afinn_df[afinn_df['word'] != 'love']
+
+# Joining on lyrics df to get word sentiment
+merged_df = pd.merge(word_df, afinn_df, on='word', how='left')
+merged_df['total_sentiment'] = merged_df['count'] * merged_df['sentiment']
+
+pd.set_option('display.max_rows', None)
+print(merged_df)
+pd.reset_option('display.max_rows')
+
+total_sentiment = merged_df['total_sentiment'].sum()
+print(f"Estimated song sentiment: {total_sentiment}")
